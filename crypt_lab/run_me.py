214658d818b37bfa4139bde7f9b6f8b0f2113053
Joseph Walton-Rivers <jwalto@essex.ac.uk>
2016-11-24 13:31:33 +0000

    origin encrypted using an unknown key and run_me for encrypting files

--- crypt_lab/run_me.py
+++ crypt_lab/run_me.py
@@ -29,6 +29,20 @@ def do_subst():
 	print("encrypted text: "+cipher_text)
 	print("decrypted text: "+decoded_text)
 
+def do_subst_file():
+	"""Substution cypher test - very basic as it only support shifts"""
+	filename = input("enter a filename (plain text): ")
+	with open(filename) as f:
+		plain_text = "".join(f.readlines())
+
+	shift_val = int(input("enter your shift value "))
+	key = rotate(string.ascii_lowercase, shift_val)
+
+	filename = input("enter a filename to save to: ")
+	cipher_text = python_crypt.encrypt_subst(plain_text, key)
+	with open(filename, "w") as f:
+		f.write(cipher_text)
+
 def subst_key():
 	"""generate a substitution cypher key"""
 	key = python_crypt.generate_key()
@@ -73,7 +87,8 @@ if __name__ == '__main__':
 		"ex2": subst_key,
 		"ex3": subst_attack,
 		"ex4": subst_attack_file,
-		"otp": do_otp
+		"otp": do_otp,
+		"file": do_subst_file
 	}
 	while True:
 		menu_choice = input("enter a choice: ")
